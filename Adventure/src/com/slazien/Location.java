5d65ab625fdcd4ceb747d8d8f542efec250ebb49
slazien <p.zientala.1995@gmail.com>
2018-01-18 12:46:44 +0000

    Added code for immutable class and small adventure game

--- Adventure/src/com/slazien/Location.java
+++ Adventure/src/com/slazien/Location.java
@@ -3,20 +3,27 @@ package com.slazien;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Location {
+
+// This class is fully immutable - note the new in constructor and getExits, as well as no setters and final+private variables
+public final class Location {
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>();
+        if (exits != null) {
+            this.exits = new HashMap<>(exits);
+        } else {
+            this.exits = new HashMap<>();
+        }
+        this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location) {
-        this.exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        this.exits.put(direction, location);
+//    }
 
     public int getLocationID() {
         return locationID;
--- Adventure/src/com/slazien/Main.java
+++ Adventure/src/com/slazien/Main.java
@@ -11,22 +11,72 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
+        Map<String, Integer> tempExit = new HashMap<>();
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExit));
+
+        tempExit = new HashMap<>();
+        // Add possible exits from each location
+        tempExit.put("W", 2);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        tempExit.put("N", 5);
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building", tempExit));
+
+        tempExit = new HashMap<>();
+        tempExit.put("N", 5);
+        locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
+
+        tempExit = new HashMap<>();
+        tempExit.put("W", 1);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExit));
+
+        tempExit = new HashMap<>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
+        locations.put(4, new Location(4, "You are in a valley beside a stream", tempExit));
+
+        tempExit = new HashMap<>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
+        locations.put(5, new Location(5, "You are in the forest", tempExit));
+
+        Map<String, String> vocab = new HashMap<>();
+        vocab.put("QUIT", "Q");
+        vocab.put("NORTH", "N");
+        vocab.put("SOUTH", "S");
+        vocab.put("EAST", "E");
+        vocab.put("WEST", "W");
 
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
+            tempExit.remove("S");
             if (loc == 0) {
                 break;
             }
 
-            loc = scanner.nextInt();
-            if (!locations.containsKey(loc)) {
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are: ");
+            for (String exit : exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if (direction.length() > 1) {
+                String[] singletons = direction.split(" ");
+                for (String singleton : singletons) {
+                    direction = vocab.get(singleton);
+                    if (direction != null) {
+                        break;
+                    }
+                }
+            }
+
+
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
                 System.out.println("You cannot go in that direction");
             }
         }
